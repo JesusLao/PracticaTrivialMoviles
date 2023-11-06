a977e9fca5ec07a779e87f26b9a0e06a7f8e5ecf
JesusLao <yisus.00@hotmail.com>
2023-11-06 18:28:12 +0100

    Imagen implementada

--- app/src/main/java/com/example/practicatrivialmoviles/MainActivity.kt
+++ app/src/main/java/com/example/practicatrivialmoviles/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.practicatrivialmoviles
 
 import android.content.Context
 import android.content.res.Configuration
+import android.content.res.Resources
+import android.graphics.drawable.shapes.Shape
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -24,6 +31,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -32,18 +40,22 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.practicatrivialmoviles.ui.theme.AppTheme
-import org.json.JSONArray
-import org.json.JSONObject
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import android.content.res.Resources
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,10 +68,13 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 data class Pregunta(
     val pregunta: String,
     val opciones: List<String>,
-    val opcion_correcta: String
+    val opcion_correcta: String,
+    var imagen: Int?, // ID de recurso de imagen
+    var videoUrl: String? // URL del video
 )
 
 @Composable
@@ -82,56 +97,18 @@ fun MyApp() {
         if (shouldShowOnboarding) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
-            /*val questions = outputJsonString.getJSONArray("preguntas") as JSONArray
-
-            for (i in 0 until questions.length()){
-                val question = questions.getJSONObject(i).getString("pregunta")
-                arrQuestions.add(question)
-
-
-
-            }*/
             val gson = Gson()
-            //val listType = object : TypeToken<List<List<String>>>() {}.type
-            val preguntas: List<Pregunta> = gson.fromJson(LocalContext.current.resources.openRawResource(R.raw.preguntas).bufferedReader().use { it.readText() }, object : TypeToken<List<Pregunta>>() {}.type)
-
+            val preguntas: List<Pregunta> = gson.fromJson(
+                LocalContext.current.resources.openRawResource(R.raw.preguntas).bufferedReader()
+                    .use { it.readText() }, object : TypeToken<List<Pregunta>>() {}.type
+            )
+            preguntas[0].imagen=R.drawable.edt
+            preguntas[1].videoUrl="https://www.youtube.com/watch?v=Ul8Xz3_r0Eg"
 
             Quiz(preguntas)
         }
     }
 }
-
-fun loadPreguntasFromJson(context: Context, fileName: String): List<Pregunta> {
-    val json: String = try {
-        val resourceId = context.resources.getIdentifier(fileName, "raw", context.packageName)
-        val jsonStream = context.resources.openRawResource(resourceId)
-        jsonStream.bufferedReader().use { it.readText() }
-    } catch (e: Resources.NotFoundException) {
-        ""
-    }
-
-    return if (json.isNotEmpty()) {
-        val gson = Gson()
-        val preguntaList: List<Pregunta> = gson.fromJson(json, Array<Pregunta>::class.java).toList()
-        preguntaList
-    } else {
-        emptyList()
-    }
-}
-
-
-/*private fun readJSONFromResource(context: Context, resourceId: Int): String {
-    val inputStream: InputStream = context.assets.open("raw/pre.json")
-    val reader = BufferedReader(InputStreamReader(inputStream))
-    val sb = StringBuilder()
-    var line: String? = reader.readLine()
-    while (line != null) {
-        sb.append(line).append('\n')
-        line = reader.readLine()
-    }
-    reader.close()
-    return sb.toString()
-}*/
 
 @Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
@@ -152,63 +129,186 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
+fun OnWinScreen(onContinueClicked: () -> Unit) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Has Ganado", style = MaterialTheme.typography.titleLarge)
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue", style = MaterialTheme.typography.labelMedium)
+        }
+    }
+}
+
+@Composable
+fun OnLoseScreen(onContinueClicked: () -> Unit/*, onReturnToOnboarding: () -> Unit*/) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Has Perdido", style = MaterialTheme.typography.titleLarge)
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue", style = MaterialTheme.typography.labelMedium)
+        }
+        /*Button(
+            modifier = Modifier.padding(vertical = 12.dp),
+            onClick = onReturnToOnboarding
+        ) {
+            Text("Return to Onboarding", style = MaterialTheme.typography.labelMedium)
+        }*/
+    }
+}
+
+@Composable
 fun Quiz(preguntas: List<Pregunta>) {
     var currentQuestionIndex by remember { mutableIntStateOf(0) }
     var selectedAnswer by remember { mutableIntStateOf(-1) }
+    var quizCompleted by remember { mutableStateOf(false) }
+    var quizFailed by remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 30.dp, vertical = 100.dp)
-            .background(MaterialTheme.colorScheme.background,MaterialTheme.shapes.extraLarge),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = preguntas[currentQuestionIndex].pregunta,
-            style = MaterialTheme.typography.titleLarge,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(40.dp)
-        ) {
-            itemsIndexed(preguntas[currentQuestionIndex].opciones) { index, answer ->
-                AnswerCard(
-                    answer = answer,
-                    isSelected = index == selectedAnswer,
-                    onAnswerSelected = {
-                        selectedAnswer = index
-                    }
-                )
+    fun nextQuestion() {
+        if (selectedAnswer >= 0) {
+            if (preguntas[currentQuestionIndex].opciones[selectedAnswer] == preguntas[currentQuestionIndex].opcion_correcta) {
+                if (currentQuestionIndex < preguntas.size - 1) {
+                    currentQuestionIndex++
+                    selectedAnswer = -1
+                } else {
+                    // Quiz completado
+                    quizCompleted = true
+                }
+            } else {
+                // Respuesta incorrecta, ir a la pantalla de pérdida
+                quizFailed = true
             }
         }
+    }
 
-        Spacer(modifier = Modifier.height(16.dp))
+    if (quizCompleted) {
+        OnWinScreen(onContinueClicked = {
+            currentQuestionIndex = 0
+            selectedAnswer = -1
+            quizCompleted = false
+            quizFailed = false
+        })
+    } else if (quizFailed) {
+        OnLoseScreen(onContinueClicked = {
+            currentQuestionIndex = 0
+            selectedAnswer = -1
+            quizCompleted = false
+            quizFailed = false
+        }/*,onReturnToOnboarding = {
+            // Esta parte llevará al usuario de regreso a OnboardingScreen
+            currentQuestionIndex = 0
+            selectedAnswer = -1
+            quizCompleted = false
+            quizFailed = false
 
-        Button(
-            onClick = {
-                if (selectedAnswer >= 0) {
-                    if (currentQuestionIndex < preguntas.size - 1) {
-                        currentQuestionIndex++
-                        selectedAnswer = -1
-                    } else {
-                        // Handle quiz completion
-                    }
-                }
-            },
-            shape = MaterialTheme.shapes.large,
-            modifier = Modifier.padding(16.dp), colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-                contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
-                disabledContainerColor = MaterialTheme.colorScheme.inversePrimary,
-                disabledContentColor = MaterialTheme.colorScheme.inversePrimary
-            ),
+        }*/)
+    }else {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.onPrimary, MaterialTheme.shapes.extraLarge),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Next Question", style = MaterialTheme.typography.labelMedium)
+            Text(
+                text = preguntas[currentQuestionIndex].pregunta,
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(horizontal = 70.dp)
+            )
+
+            preguntas[currentQuestionIndex].imagen?.let { imagenId ->
+                Image(
+                    painter = painterResource(imagenId),
+                    contentDescription = "Imagen de la pregunta",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                )
+            }
+
+            //preguntas[currentQuestionIndex].videoUrl?.let { videoUrl ->
+                /*val exoPlayer=ExoPlayer.Builder(LocalContext.current).build()
+                val mediaItem = MediaItem.fromUri(Uri.parse(videoUrl))
+                exoPlayer.setMediaItem(mediaItem)
+
+                val playerView = StyledPlayerView(LocalContext.current)
+                playerView.player=exoPlayer
+                
+                DisposableEffect(AndroidView(factory = {playerView})){
+                    exoPlayer.prepare()
+                    exoPlayer.playWhenReady=true
+
+                    onDispose { exoPlayer.release() }
+                }*/
+
+
+
+                /*AndroidView(
+                    factory = { context ->
+                        WebView(context).apply {
+                            layoutParams = ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT
+                            )
+                            settings.javaScriptEnabled = true
+                        }
+                    },
+                    update = { webView ->
+                        webView.loadUrl(videoUrl)
+                    }
+                )*/
+            //}
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.onPrimary, MaterialTheme.shapes.extraLarge)
+                    .padding(40.dp)
+            ) {
+                itemsIndexed(preguntas[currentQuestionIndex].opciones) { index, answer ->
+                    AnswerCard(
+                        answer = answer,
+                        isSelected = index == selectedAnswer,
+                        onAnswerSelected = {
+                            selectedAnswer = index
+                        }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    nextQuestion()
+
+
+                },
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier.padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                    disabledContainerColor = MaterialTheme.colorScheme.inversePrimary,
+                    disabledContentColor = MaterialTheme.colorScheme.inversePrimary
+                ),
+            ) {
+                Text(text = "Next Question", style = MaterialTheme.typography.labelMedium)
+            }
         }
     }
 }
